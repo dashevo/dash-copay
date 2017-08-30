983733db41fcb601da36f00a8543acfcc5c49983
Gustavo Maximiliano Cortez <cmgustavo83@gmail.com>
2017-08-30 16:38:42 -0300

    Adds setting for bitcoin cash support

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -63,7 +63,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
     },
 
     cashSupport: {
-      enabled: true,
+      enabled: false,
     },
 
     recentTransactions: {
