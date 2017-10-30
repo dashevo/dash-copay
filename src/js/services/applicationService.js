9003c7324a4ac9ab412d33806b95ba167c851547
Jake <jake@ideasbynature.com>
2017-10-30 09:21:46 -0600

    fixed fingerscanner

--- src/js/services/applicationService.js
+++ src/js/services/applicationService.js
@@ -50,20 +50,21 @@ angular.module('copayApp.services')
       });
       scope.openModal = function() {
         scope.fingerprintCheckModal.show();
-        checkFingerprint();
+        scope.checkFingerprint();
       };
       scope.hideModal = function() {
         root.isModalOpen = false;
         scope.fingerprintCheckModal.hide();
       };
 
-      function checkFingerprint() {
+      scope.checkFingerprint = function() {
         fingerprintService.check('unlockingApp', function(err) {
           if (err) {
-            checkFingerprint();
             return;
           }
-          scope.hideModal();
+          $timeout(function() {
+            scope.hideModal();
+          }, 200);
         });
       }
     };
