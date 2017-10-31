f294289f231cb8c02e309723ad18c81af521740e
Jake <jake@ideasbynature.com>
2017-10-31 07:34:50 -0600

    making derivation path default to testnet version

--- src/js/services/derivationPathHelper.js
+++ src/js/services/derivationPathHelper.js
@@ -3,7 +3,8 @@
 angular.module('copayApp.services').factory('derivationPathHelper', function(lodash) {
   var root = {};
 
-  root.default = "m/44'/5'/0'";
+  // root.default = "m/44'/5'/0'";  Real default.  Disabling for Testnet version
+  root.default = "m/44'/1'/0'";
   root.defaultTestnet = "m/44'/1'/0'";
 
   root.parse = function(str) {
@@ -33,7 +34,7 @@ angular.module('copayApp.services').factory('derivationPathHelper', function(lod
     };
 
     switch (arr[2]) {
-      case "0'":
+      case "5'":
         ret.networkName = 'livenet';
         break;
       case "1'":
